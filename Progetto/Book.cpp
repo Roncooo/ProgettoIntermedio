283a99feea1ba74ec185d399460fbb7476901262
Francesco <francesco.roncolato@studenti.unipd.it>
2022-11-16 17:33:42 +0100

    Completata classe Isbn, da testare

--- Progetto/Book.cpp
+++ Progetto/Book.cpp
@@ -1,10 +1,49 @@
 #include "Book.h"
 
-Book::Book()
-{
-}
+//Book::Book()
+//{
+//}
+//
+//Book::~Book()
+//{
+//}
 
-Book::~Book()
+// costruttore, throws Invalid ISBN code
+Book::Isbn::Isbn(std::string code)
 {
+	if(is_valid_isbn(code))
+		isbn_code = code;
+	else
+		throw InvalidIsbnException();
+} 
+	
+// nnn-nnn-nnn-x
+// x lettera o numero ma non carattere speciale
+// ci sono un po' di maginumbers, dite che va bene lo stesso?
+bool Book::Isbn::is_valid_isbn(std::string s)
+{
+	char dlm = '-';
+	
+	if(s.length()!=13)
+		return false;
+	
+	for(int i=0; i<3; i++)
+	{
+		if( (!isdigit(s[i])) || (!isdigit(s[i+4])) || (!isdigit(s[i+8])) )
+			return false;
+	}
+	
+	if( s[3]!=dlm || s[3+4]!=dlm || s[3+4]!=dlm )
+		return false;
+	
+	char x = s[12];
+	if( 
+		!(x>=48 && x<=57) && // x is not a number digit
+		!(x>=65 && x<=90) && // x is not a capital letter
+		!(x>=97 && x<=122)   // x is not a lowercase letter
+	  )
+		return false;
+	
+	// else
+	return true;
 }
-
--- Progetto/Book.h
+++ Progetto/Book.h
@@ -8,39 +8,46 @@ class Book
 {
 public:
 		
-		// COSTRUTTORI
-		// con tutti i parametri
-		
-		
-		// getters
+	// COSTRUTTORI
+	// con tutti i parametri
+	
+	
+	// getters
 
-		// cambiano lo stato di is_available se ha senso
-		void borrow_book(); 	// throws ImpossibleToBorrowUnvailableBook
-		void return_book();		// throws ImpossibleToReturnAvailableBook
+	// cambiano lo stato di is_available se ha senso
+	void borrow_book(); 	// throws ImpossibleToBorrowUnvailableBook
+	void return_book();		// throws ImpossibleToReturnAvailableBook
 		
 		
 
 // Ghidoni in Rational non mette il distruttore	
 //	~Book();
 private:
+	
+	class Isbn
+	{
+	private:
+		Isbn(std::string code); // throws Invalid ISBN code
+		bool is_valid_isbn(std::string code);
+			// non ha senso che venga chiamato su un oggetto Isbn, meglio se prende una stringa e controlla quella
+			// quindi è "statico"
+			// nnn-nnn-nnn-x  con x lettera o numero ma non carattere speciale
+		// variabile membro
+		std::string isbn_code;
 		
-		class Isbn
-		{
-				Isbn(std::string code); // throws Invalid ISBN code
-				bool is_valid();
-						// nnn-nnn-nnn-c
-						// c lettera o numero ma non carattere speciale
-		};
-		
-		
-		// variabile
-		Isbn isbn_code;
-		std::string title;
-		std::string auth_name;
-		std::string auth_surname;
-		Date copyright;
-		bool availability;
-		
+		// è giusto dichiararla qui? o meglio fuori da isbn?
+		class InvalidIsbnException{};
+	};
+	
+	
+	// variabile
+	Isbn isbn_code;
+	std::string title;
+	std::string auth_name;
+	std::string auth_surname;
+	Date copyright;
+	bool availability;
+	
 };
 
 // helper function
@@ -48,6 +55,6 @@ private:
 // Ghidoni le ha messe fuori
 bool operator==(Book b, Book c);
 bool operator!=(Book b, Book c);
-&ostream operator<<(Book b); 
+std::ostream& operator<<(std::ostream& os, Book b); 
 
 #endif // BOOK_HPP
