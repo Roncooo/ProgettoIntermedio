b145584a51505dce49baedc2b5b9dc2227726827
Francesco <francesco.roncolato@studenti.unipd.it>
2022-11-18 22:39:25 +0100

    Roba di Codelite

--- Progetto/Progettino.mk
+++ Progetto/Progettino.mk
@@ -6,19 +6,19 @@
 ProjectName            :=Progettino
 ConfigurationName      :=Debug
 WorkspaceConfiguration :=Debug
-WorkspacePath          :=C:/Users/fulvi/OneDrive/Desktop/codelite/GitHub
-ProjectPath            :=C:/Users/fulvi/OneDrive/Desktop/codelite/GitHub/Progetto
+WorkspacePath          :=C:/Users/franc/Desktop/ProgettoIntermedio
+ProjectPath            :=C:/Users/franc/Desktop/ProgettoIntermedio/Progetto
 IntermediateDirectory  :=../build-$(WorkspaceConfiguration)/Progetto
 OutDir                 :=$(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=fulvi
-Date                   :=18/11/2022
+User                   :=Francesco
+Date                   :=11/18/2022
 CodeLitePath           :="C:/Program Files/CodeLite"
 MakeDirCommand         :=mkdir
-LinkerName             :=C:/Users/fulvi/Downloads/winlibs-x86_64-posix-seh-gcc-12.2.0-llvm-14.0.6-mingw-w64ucrt-10.0.0-r2/mingw64/bin/g++.exe
-SharedObjectLinkerName :=C:/Users/fulvi/Downloads/winlibs-x86_64-posix-seh-gcc-12.2.0-llvm-14.0.6-mingw-w64ucrt-10.0.0-r2/mingw64/bin/g++.exe -shared -fPIC
+LinkerName             :="C:/Program Files/mingw64/bin/g++.exe"
+SharedObjectLinkerName :="C:/Program Files/mingw64/bin/g++.exe" -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -29,7 +29,7 @@ OutputSwitch           :=-o
 LibraryPathSwitch      :=-L
 PreprocessorSwitch     :=-D
 SourceSwitch           :=-c 
-OutputDirectory        :=C:/Users/fulvi/OneDrive/Desktop/codelite/GitHub/build-$(WorkspaceConfiguration)/bin
+OutputDirectory        :=C:/Users/franc/Desktop/ProgettoIntermedio/build-$(WorkspaceConfiguration)/bin
 OutputFile             :=..\build-$(WorkspaceConfiguration)\bin\$(ProjectName)
 Preprocessors          :=
 ObjectSwitch           :=-o 
@@ -38,7 +38,7 @@ PreprocessOnlySwitch   :=-E
 ObjectsFileList        :=$(IntermediateDirectory)/ObjectsList.txt
 PCHCompileFlags        :=
 RcCmpOptions           := 
-RcCompilerName         :=C:/Users/fulvi/Downloads/winlibs-x86_64-posix-seh-gcc-12.2.0-llvm-14.0.6-mingw-w64ucrt-10.0.0-r2/mingw64/bin/windres.exe
+RcCompilerName         :="C:/Program Files/mingw64/bin/windres.exe"
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -51,20 +51,20 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overridden using an environment variable
 ##
-AR       := C:/Users/fulvi/Downloads/winlibs-x86_64-posix-seh-gcc-12.2.0-llvm-14.0.6-mingw-w64ucrt-10.0.0-r2/mingw64/bin/ar.exe rcu
-CXX      := C:/Users/fulvi/Downloads/winlibs-x86_64-posix-seh-gcc-12.2.0-llvm-14.0.6-mingw-w64ucrt-10.0.0-r2/mingw64/bin/g++.exe
-CC       := C:/Users/fulvi/Downloads/winlibs-x86_64-posix-seh-gcc-12.2.0-llvm-14.0.6-mingw-w64ucrt-10.0.0-r2/mingw64/bin/gcc.exe
+AR       := "C:/Program Files/mingw64/bin/ar.exe" rcu
+CXX      := "C:/Program Files/mingw64/bin/g++.exe"
+CC       := "C:/Program Files/mingw64/bin/gcc.exe"
 CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := C:/Users/fulvi/Downloads/winlibs-x86_64-posix-seh-gcc-12.2.0-llvm-14.0.6-mingw-w64ucrt-10.0.0-r2/mingw64/bin/as.exe
+AS       := "C:/Program Files/mingw64/bin/as.exe"
 
 
 ##
 ## User defined environment variables
 ##
 CodeLiteDir:=C:\Program Files\CodeLite
-Objects0=$(IntermediateDirectory)/Book.cpp$(ObjectSuffix) $(IntermediateDirectory)/Date.cpp$(ObjectSuffix) $(IntermediateDirectory)/tester.cpp$(ObjectSuffix) 
+Objects0=$(IntermediateDirectory)/Book.cpp$(ObjectSuffix) $(IntermediateDirectory)/tester.cpp$(ObjectSuffix) $(IntermediateDirectory)/Date.cpp$(ObjectSuffix) 
 
 
 
@@ -96,28 +96,28 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/Book.cpp$(ObjectSuffix): Book.cpp $(IntermediateDirectory)/Book.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/fulvi/OneDrive/Desktop/codelite/GitHub/Progetto/Book.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Book.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/franc/Desktop/ProgettoIntermedio/Progetto/Book.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Book.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/Book.cpp$(DependSuffix): Book.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Book.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Book.cpp$(DependSuffix) -MM Book.cpp
 
 $(IntermediateDirectory)/Book.cpp$(PreprocessSuffix): Book.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Book.cpp$(PreprocessSuffix) Book.cpp
 
-$(IntermediateDirectory)/Date.cpp$(ObjectSuffix): Date.cpp $(IntermediateDirectory)/Date.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/fulvi/OneDrive/Desktop/codelite/GitHub/Progetto/Date.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Date.cpp$(ObjectSuffix) $(IncludePath)
-$(IntermediateDirectory)/Date.cpp$(DependSuffix): Date.cpp
-	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Date.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Date.cpp$(DependSuffix) -MM Date.cpp
-
-$(IntermediateDirectory)/Date.cpp$(PreprocessSuffix): Date.cpp
-	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Date.cpp$(PreprocessSuffix) Date.cpp
-
 $(IntermediateDirectory)/tester.cpp$(ObjectSuffix): tester.cpp $(IntermediateDirectory)/tester.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/fulvi/OneDrive/Desktop/codelite/GitHub/Progetto/tester.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/tester.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/franc/Desktop/ProgettoIntermedio/Progetto/tester.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/tester.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/tester.cpp$(DependSuffix): tester.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/tester.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/tester.cpp$(DependSuffix) -MM tester.cpp
 
 $(IntermediateDirectory)/tester.cpp$(PreprocessSuffix): tester.cpp
 	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/tester.cpp$(PreprocessSuffix) tester.cpp
+
+$(IntermediateDirectory)/Date.cpp$(ObjectSuffix): Date.cpp $(IntermediateDirectory)/Date.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Users/franc/Desktop/ProgettoIntermedio/Progetto/Date.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/Date.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/Date.cpp$(DependSuffix): Date.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/Date.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/Date.cpp$(DependSuffix) -MM Date.cpp
+
+$(IntermediateDirectory)/Date.cpp$(PreprocessSuffix): Date.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/Date.cpp$(PreprocessSuffix) Date.cpp
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
