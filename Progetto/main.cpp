45dcf6357d4e39524db0dbf7b0c67b3469f7023c
Fulvio <fulvio.bruzzese@studenti.unipd.it>
2022-11-20 17:47:36 +0100

    rimesso il for (bea vai più spesso a capo pls)

--- Progetto/main.cpp
+++ Progetto/main.cpp
@@ -27,20 +27,26 @@ int main(void)
 		
 		// mettendo una data precedente al 1/1/52 giustamente lancia eccezione
 		Book book_im_reading = Book("Hannah", "Arendt", "La banalita' del male", "888-222-444-r", Date(6, 8, 1997), false);
-		cout << "Libro creato: " << book_im_reading << "\n";
+		cout << "Libro creato: " << book_im_reading << "\n" << endl;
+		
 		// ritorno book_im_reading
 		cout << "*** Processo di restituzione del libro ***\n";
 		book_im_reading.return_book();
-		cout << "Hai restituito: " << book_im_reading <<"\n";
-		cout << book_im_reading.get_title() << " di " << book_im_reading.get_auth_surname() <<" disponibile per il prestito\n";
+		cout << "Hai restituito: " << book_im_reading <<"\n" << endl;
+		cout << book_im_reading.get_title() << " di " << book_im_reading.get_auth_surname() <<" disponibile per il prestito\n" << endl;
+		
+		//libro 2
+		cout << "Creo e stampo un altro libro" << endl;
+		Book bd_book = Book ("Matt", "Fraction", "Fear Itself", "882-871-134-6", Date(1,2,1953));
+		cout << bd_book << endl << endl;
 		
 		// creo e prendo in prestito un libro
 		Book book_i_want = Book("Stephen W.", "Hawking", "La teoria del tutto","978-881-707-3", Date(1, 1, 1998));
-		cout << "Libro creato: " << book_i_want << "\n";
+		cout << "Libro creato: " << book_i_want << "\n" << endl;
 		cout << "*** Processo di presa in prestito del libro ***\n";
 		
 		book_i_want.borrow_book();
-		cout << "Hai preso in prestito: " << book_i_want <<"\n";
+		cout << "Hai preso in prestito: " << book_i_want <<"\n" << endl;
 		
 		// provo a confrontare i libri creati in precedenza
 		cout << "Il libro restituito e quello preso in prestito sono uguali?\n";
@@ -54,9 +60,9 @@ int main(void)
 		cout << "Il libro di prova: " << book_prova <<"\n";
 		cout << "Il libro di prova e " << last_book_read.get_title() << " sono uguali?\n";
 		if(book_prova == last_book_read)
-			cout << "I due libri sono uguali\n";
+			cout << "I due libri sono uguali\n" << endl;
 		else
-			cout << "I due libri non sono uguali\n";
+			cout << "I due libri non sono uguali\n" << endl;
 		
 		//	dati
 		string title = "Una cosa divertente che non faro' mai piu'";
@@ -71,6 +77,10 @@ int main(void)
 		//creo un vettore di book
 		vector<Book> shelf(10);
 		cout << "Vector di Book creato\n";
+		//controllo
+		for (int i = 0; i < shelf.size(); i++)
+			cout << shelf[i] << endl;
+			
 	}
 	catch(Date::InvalidDateException e) { cerr << "***ERRORE***: hai creato una data invalida!\n"; }
 	catch(Book::InvalidCopyrightDateException e) { cerr << "***ERRORE***: hai creato una data invalida per il copyright!\n"; }
